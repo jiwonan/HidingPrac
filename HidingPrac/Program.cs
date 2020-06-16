c1cdc5b2bffaa2875aa4b865b6123987b8a3cc9d
Jiwon An <s2018s31@e-mirim.hs.kr>
2020-06-16 09:45:49 +0900

    ovrride 키워드

--- HidingPrac/Program.cs
+++ HidingPrac/Program.cs
@@ -16,6 +16,10 @@ namespace HidingPrac
             {
                 Console.WriteLine("Parent!");
             }
+            public virtual void Method2()
+            {
+                Console.WriteLine("Parent!");
+            }
         }
         class Child : Parent
         {
@@ -24,16 +28,26 @@ namespace HidingPrac
             {
                 Console.WriteLine("Child!");
             }
+
+            public override void Method2()
+            {
+                Console.WriteLine("Child!");
+            }
         }
 
         static void Main(string[] args)
         {
             Child c = new Child();
+            Console.WriteLine("하이딩");
             Console.WriteLine("variable : " + c.variable);
             Console.WriteLine("Parent variable : " + ((Parent)c).variable);
 
-            c.Method();
+            c.Method(); // 부모의 메서드를 가림. (부모의 메서드 남아있음)
             ((Parent)c).Method();
+
+            Console.WriteLine("오버라이딩");
+            c.Method2(); // 부모의 메서드를 덮어씀. (부모의 메서드 사라짐)
+            ((Parent)c).Method2();  
         }
     }
 }
